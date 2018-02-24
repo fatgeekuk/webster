a63bcdf59a657038945a0f300a76b0a44fbd8901
Peter Morris <peter.r.morris@gmail.com>
2018-02-24 14:45:39 +0000

    chore(file uploads): now working

--- backend/lib/backend_web/controllers/upload_signature_controller.ex
+++ backend/lib/backend_web/controllers/upload_signature_controller.ex
@@ -10,7 +10,8 @@ defmodule BackendWeb.UploadSignatureController do
 
   defp sign(filename, mimetype) do
     policy = policy(filename, mimetype)
-
+    signing_key = signing_key();
+    Logger.info "signing key #{ signing_key }";
     %{
       key: filename,
       'Content-Type': mimetype,
@@ -19,7 +20,7 @@ defmodule BackendWeb.UploadSignatureController do
       action: "https://s3.amazonaws.com/#{System.get_env("S3_BUCKET_NAME")}",
       'AWSAccessKeyId': System.get_env("AWS_ACCESS_KEY_ID"),
       policy: policy,
-      signature: hmac_sha1(System.get_env("AWS_SECRET_ACCESS_KEY"), policy)
+      signature: hmac_sha1(signing_key, policy)
     }
   end
 
@@ -30,13 +31,33 @@ defmodule BackendWeb.UploadSignatureController do
     |> format!("{ISO:Extended:Z}")
   end
 
+  defp signing_key() do
+    s1key = Timex.format!(Timex.now(), "%Y%m%d", :strftime);
+    s2key = "eu-west-2";
+    s3key = "s3";
+    s4key = "aws4_request";
+
+    #  Iterates over the data (defined in the array above), hashing it each time.
+    initial = "AWS4#{ System.get_env("AWS_SECRET_ACCESS_KEY") }";
+    Logger.info "initial #{ initial }";
+    Logger.info "s1 #{s1key}";
+    step1 = :crypto.hmac(:sha256, initial, s1key);
+    step2 = :crypto.hmac(:sha256, step1, s2key);
+    step3 = :crypto.hmac(:sha256, step2, s3key);
+    signing_key = :crypto.hmac(:sha256, step3, s4key);
+
+    signing_key
+  end
+
   defp hmac_sha1(secret, msg) do
 
-   Logger.info  "Logging this text!"
-        Logger.debug "Var value: #{inspect(msg)}"
-              Logger.debug "Var valueq: #{inspect(secret)}"
-    :crypto.hmac(:sha256, secret, msg)
-    |> Base.encode64
+    Logger.info  "Logging this text!"
+    Logger.debug "Var value: #{inspect(msg)}"
+    Logger.debug "Var valueq: #{inspect(secret)}"
+
+    qq = :crypto.hmac(:sha256, secret, msg)
+    Logger.info "signed dingus #{ qq }";
+    qq |> Base.encode16(case: :lower)
   end
 
   defp policy(key, mimetype, expiration_window \\ 60) do
@@ -50,10 +71,13 @@ defmodule BackendWeb.UploadSignatureController do
         %{acl: "public-read"},
         # You have to upload the mime type you said you would upload.
         ["starts-with", "$Content-Type", mimetype],
+        ["starts-with", "$x-amz-date", "2018"],
+        ["starts-with", "$x-amz-algorithm", "AWS4-HMAC-SHA256"],
+        ["starts-with", "$x-amz-credential", System.get_env("AWS_ACCESS_KEY_ID")],
         # You have to upload the file name you said you would upload.
         ["starts-with", "$key", key],
         # When things work out ok, AWS should send a 201 response.
-        %{success_action_status: "201"}
+        #%{success_action_status: "201"}
       ]
     }
     |> Poison.encode!
--- frontend/scripts/models/file_upload.js
+++ frontend/scripts/models/file_upload.js
@@ -51,23 +51,46 @@ class FileUpload {
     formData.append('policy', this.response.policy);
     formData.append('x-amz-algorithm', 'AWS4-HMAC-SHA256');
     formData.append('x-amz-credential', this.response.AWSAccessKeyId + '/' + nDate + '/eu-west-2/s3/aws4_request');
-    formData.append('x-amz-date', nDateFull);
     formData.append('x-amz-signature', this.response.signature);
+    formData.append('x-amz-date', nDateFull);
     formData.append('acl', 'public-read');
-    //formData.append('contentType', this.type);
+    formData.append('Content-Type', this.type);
 
     console.log('file', this.file);
     formData.append('file', this.file);
     
     var bucket = 'development-store.webster-music.org';
-    var endpoint = 'http://' + bucket + '.s3.amazonaws.com';
-    $.ajax(endpoint, {
+    var region = 'eu-west-2';
+    //var endpoint = 'http://' + bucket + '.s3.amazonaws.com';
+    var endpoint = 'https://s3-' + region + '.amazonaws.com/' + bucket;
+    $.ajax({
+      url: endpoint,
+      xhr: function() {
+        var xhr = new window.XMLHttpRequest();
+        xhr.upload.addEventListener("progress", function(evt) {
+            if (evt.lengthComputable) {
+                var percentComplete = evt.loaded / evt.total;
+                //Do something with upload progress here
+                console.log('progress!', percentComplete);
+            }
+       }, false);
+
+       xhr.addEventListener("progress", function(evt) {
+           if (evt.lengthComputable) {
+               var percentComplete = evt.loaded / evt.total;
+               //Do something with download progress
+               console.log('progress!', percentComplete);
+           }
+       }, false);
+
+       return xhr;
+    },
       method: 'POST',
       processData: false,
       contentType: false,
       data: formData,
       success: function(responseqq){
-        console.log(responseqq)
+        console.log('yabba dabba doo', responseqq)
       }
     })
   }
